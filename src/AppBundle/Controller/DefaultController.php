b50ee2f77e6084fc6bd221da4f829e2fb35e9d8e
Arkadiusz <sienkoarkadiusz@gmail.com>
2016-02-09 22:28:02 +0100

    Added Car Entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Client;
+use AppBundle\Form\Type\ClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -18,6 +21,21 @@ class DefaultController extends Controller
 
         var_dump($day);
         die;
+    }
+    public function bookAction(Request $request)
+    {
+        $client = new Client();
+
+        $form = $this->createForm(ClientType::class, $client);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+        }
+
+        return $this->render('AppBundle:default:booking.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
 
